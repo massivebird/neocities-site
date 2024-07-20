abca6dee7999e51590b58cea6ddd855c443ef3a7
massivebird <gdrakemail@gmail.com>
2024-07-20 14:16:14 -0400

    removed: unused flake inputs

--- flake.nix
+++ flake.nix
@@ -3,16 +3,13 @@
 
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.11";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
       in
       with pkgs;
